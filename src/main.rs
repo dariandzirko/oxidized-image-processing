b66e16fbb4221386b42e2f09fa7b92dbaac34acd
darian <dariandzirko@gmail.com>
2022-03-19 14:25:18 -0600

    I just got standard deviation working. That was a trivial translation from matlab to rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use core::panic;
-use std::{f32::consts::E, io::BufRead, env::temp_dir, num::IntErrorKind};
+use std::{f32::consts::E, io::BufRead, env::temp_dir, num::IntErrorKind, vec};
 use image::{GenericImageView, DynamicImage, RgbImage, Rgb, ImageBuffer, Luma, GrayImage, Pixel, Rgba};
-use num;
+use num::{self, pow, Float};
 
 fn main() {
     // Use the open function to load an image from a Path.
@@ -27,8 +27,12 @@ fn main() {
     // let integral_image_boy = integral_image(&img);
     // integral_image_boy.save("integral_image_boy.png").unwrap();
 
-    let haar_image_boy = haar_filter(&img, 20, 25);
-    haar_image_boy.save("haar_image_boy.png").unwrap();
+    // let haar_image_boy = haar_filter(&img, 20, 25);
+    // haar_image_boy.save("haar_image_boy.png").unwrap();
+
+    let (standard_dev_boy, mean_boy) = local_statistics(&img, 3, 3);
+    standard_dev_boy.save("standard_dev_boy.png").unwrap();
+    mean_boy.save("mean_boy.png").unwrap();
 
 }
 
@@ -318,16 +322,16 @@ fn integral_image(base: &GrayImage) -> Vec<Vec<f32>> {
     let (base_cols, base_rows) = base.dimensions();
 
     let mut result = GrayImage::new(base_cols, base_rows);
-    let mut value_holder: Vec<Vec<f32>> = vec![vec![0.0;base_cols as usize+1];base_rows as usize+1];
+    let mut value_holder: Vec<Vec<f32>> = vec![vec![0.0;(base_cols+1) as usize];(base_rows+1) as usize];
 
     let mut max: f32 = 0.0;
 
     for row in 1..base_rows+1 {
         for col in 1..base_cols+1 {
             value_holder[row as usize][col as usize] = *base.get_pixel(col-1, row-1).channels().get(0).unwrap() as f32
-                                                        + value_holder[row as usize][col as usize-1] 
-                                                        + value_holder[row as usize-1][col as usize]
-                                                        - value_holder[row as usize-1][col as usize-1];
+                                                        + value_holder[row as usize][(col-1) as usize] 
+                                                        + value_holder[(row-1) as usize][col as usize]
+                                                        - value_holder[(row-1) as usize][(col-1) as usize];
 
             if value_holder[row as usize][col as usize] > max {
                 max = value_holder[row as usize][col as usize]
@@ -349,6 +353,29 @@ fn integral_image(base: &GrayImage) -> Vec<Vec<f32>> {
 
     return value_holder;
 }
+
+// This is a bad solution but fuck em for the time being
+fn integral_image_matrix(base: Vec<Vec<f32>> ) -> Vec<Vec<f32>> {
+
+    let base_rows = base.len();
+    let base_cols = base[0].len();
+
+    let mut value_holder: Vec<Vec<f32>> = vec![vec![0.0;(base_cols+1) as usize];(base_rows+1) as usize];
+
+    for row in 1..base_rows+1 {
+        for col in 1..base_cols+1 {
+            value_holder[row as usize][col as usize] = base[(row-1) as usize][(col-1) as usize]
+                                                        + value_holder[row as usize][(col-1) as usize] 
+                                                        + value_holder[(row-1) as usize][col as usize]
+                                                        - value_holder[(row-1) as usize][(col-1) as usize];
+
+        }
+    }
+
+    return value_holder;
+}
+
+
 
 fn haar_filter(base: &GrayImage, Mh:u32, Mv:u32) -> GrayImage {
     let (base_cols, base_rows) = base.dimensions();
@@ -407,4 +434,144 @@ fn haar_filter(base: &GrayImage, Mh:u32, Mv:u32) -> GrayImage {
     }
     
     return result;
+}
+
+fn image_raised_power(base: &GrayImage, power: f32 ) -> Vec<Vec<f32>>{ 
+    let (base_cols, base_rows) = base.dimensions();
+
+    let mut float_result = vec![vec![0.0;base_cols as usize]; base_rows as usize];
+
+    for row in 0..base_rows {
+        for col in 0..base_cols{
+            float_result[row as usize][col as usize] = (*base.get_pixel(col, row).channels().get(0).unwrap() as f32).powf(power);
+        }
+    }
+
+    return float_result;
+}
+
+// Return order is standard dev iamge first and mean image second
+fn local_statistics(base: &GrayImage, window_height: u32, window_width: u32) -> (GrayImage, GrayImage) {
+
+    let(base_cols, base_rows) = base.dimensions();
+
+    let mut zero_pad_base = GrayImage::new(base_cols+window_width, base_rows+window_height);
+
+    image::imageops::overlay(&mut zero_pad_base, base, ((window_width+1)/2) as i64, ((window_height+1)/2) as i64);
+
+    let integral = integral_image(&zero_pad_base);
+
+    let base_squared = image_raised_power(&zero_pad_base, 2.0);
+    let integral_squared = integral_image_matrix(base_squared);
+
+    let mut mean_value_holder: Vec<Vec<f32>> = vec![vec![0.0;base_cols as usize];base_rows as usize];
+    let mut standard_dev_value_holder: Vec<Vec<f32>> = vec![vec![0.0;base_cols as usize];base_rows as usize];
+
+    let mut result_mean = GrayImage::new(base_cols, base_rows);
+    let mut result_standard_dev = GrayImage::new(base_cols, base_rows);
+
+    let mut num_of_elements_in_window;
+    let mut window_sum;
+    let mut squared_window_sum;
+
+    let mut window_mean;
+    let mut squared_window_mean;
+
+    let mut sigma;
+    let mut sigma_squared;
+
+    let mut mean_max = 0.0;
+    let mut standard_dev_max = 0.0;
+
+    for row in 0..base_rows {
+        for col in 0..base_cols {
+
+            num_of_elements_in_window = window_height*window_width;
+
+            if row < (window_height/2) {
+
+                num_of_elements_in_window -= (window_height)/2-row*window_height;
+
+                if col < (window_width/2) {
+                    num_of_elements_in_window -= (window_width/2-col)*(window_width-window_height/2-row);
+                }
+
+                if row > base_cols - window_height/2 {
+                    num_of_elements_in_window -= (col-base_cols+window_width/2)*(window_width-window_height/2-row);
+                }
+            }
+
+            else if row > base_rows - window_height/2 {
+
+                num_of_elements_in_window -= (row-base_rows+window_height/2)*window_height;
+
+                if col < window_width/2 {
+                    num_of_elements_in_window -= (window_width/2-col)*(window_width-(row-base_rows+window_height/2));
+                }
+
+                if col > base_cols - window_height/2 {
+                    num_of_elements_in_window -= (col-base_cols+window_width/2)*(window_width-(row-base_rows+window_height/2));
+                }
+            }
+
+            else {
+                if col < window_width/2 {
+                    num_of_elements_in_window -= (window_width/2-col)*window_width
+                }
+
+                if col > base_cols - window_height/2 {
+                    num_of_elements_in_window -= (col-base_cols+window_width/2)*window_width;
+                }
+            }
+
+            window_sum = integral[ (row + window_height) as usize][ (col+window_width) as usize]
+                            - integral[row as usize][ (col+window_width) as usize]
+                            - integral[ (row + window_height) as usize][col as usize]
+                            + integral[row as usize][col as usize];
+
+            squared_window_sum = integral_squared[ (row + window_height) as usize][ (col+window_width) as usize]
+                                - integral_squared[row as usize][ (col+window_width) as usize]
+                                - integral_squared[ (row + window_height) as usize][col as usize]
+                                + integral_squared[row as usize][col as usize];
+
+            window_mean = window_sum/num_of_elements_in_window as f32;
+
+            squared_window_mean = squared_window_sum/num_of_elements_in_window as f32;
+
+            sigma_squared = squared_window_mean - window_mean.powf(2.0);
+
+            sigma = sigma_squared.sqrt();
+
+            if window_mean > mean_max {
+                mean_max = window_mean;
+            } 
+
+            if sigma > standard_dev_max {
+                standard_dev_max = sigma;
+            }
+
+            mean_value_holder[row as usize][col as usize] = window_mean;
+            standard_dev_value_holder[row as usize][col as usize] = sigma;
+
+        }
+    }
+
+    mean_value_holder.iter_mut()
+    .flat_map(|row| row.iter_mut())
+    .for_each(|item| *item = *item/mean_max*255.0);
+
+    standard_dev_value_holder.iter_mut()
+    .flat_map(|row| row.iter_mut())
+    .for_each(|item| *item = *item/standard_dev_max*255.0);
+
+    for row in 0..base_rows {
+        for col in 0..base_cols {
+            let mean_pixel: image::Luma::<u8> = image::Luma::<u8>([mean_value_holder[row as usize][col as usize] as u8]);
+            let standard_dev_pixel: image::Luma::<u8> = image::Luma::<u8>([standard_dev_value_holder[row as usize][col as usize] as u8]);
+            
+            result_mean.put_pixel(col, row, mean_pixel);
+            result_standard_dev.put_pixel(col, row,standard_dev_pixel)        }
+    }
+
+    return (result_standard_dev, result_mean);
 }
