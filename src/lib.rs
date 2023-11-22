064c4cf36528c9e5fa9870a3ffcda4117c16a6aa
Darian Dzirko <57810174+dariandzirko@users.noreply.github.com>
2023-11-22 15:17:26 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 pub mod float_image;
 pub mod kernel;
-=======
 use core::panic;
 use image::{
     DynamicImage, GenericImageView, GrayImage, ImageBuffer, Luma, Pixel, Rgb, RgbImage, Rgba,
@@ -680,4 +678,3 @@ pub fn subtract_images(
         return Err(std::io::ErrorKind::InvalidInput.into());
     }
 }
->>>>>>> main
